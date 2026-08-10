15f21111c6e4c3a9bba2b61bed20252739a6e78a
Owner <owner@Anthony.local>
2026-08-10 22:35:27 +1000

    Added all global healthy meals to schema and updated meals.db

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 Project Title, the URL of your video (created in step 1 above) and a description of your project. You may use the below as a template.
 
-sqilte3 meals.db to open databasesq
+sqilte3 meals.db to open databases
 to quit out of sqlite> type: .quit
+
+
+For github, when you ubdate your file and you need to save: git add .
+git commit -m "Updated HTML templates and routes"
+git push
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from cs50 import SQL
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 db = SQL("sqlite:///meals.db")
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Query all meals from the database
+    meals = db.execute("SELECT * FROM meals")
+    return render_template("index.html", meals=meals)
